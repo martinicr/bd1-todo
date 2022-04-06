7b4c1f6253cef94d61f15102b83dca53801afc67
Martin Flores <mfloresg@gmail.com>
2022-04-05 21:06:25 -0600

    se debe de establecer un id por default

--- bd1-todo-core/src/main/java/tec/bd/todo/Todo.java
+++ bd1-todo-core/src/main/java/tec/bd/todo/Todo.java
@@ -5,6 +5,7 @@ import tec.bd.todo.repository.TodoRepository;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 public class Todo {
 
@@ -27,12 +28,13 @@ public class Todo {
         return this.todoRepository.findById(id);
     }
 
-    public TodoRecord add(TodoRecord record) {
+    public TodoRecord addTodoRecord(TodoRecord record) {
         Objects.requireNonNull(record);
         // Si el titulo es nulo, devolver exception
         if(null == record.getStartDate()) {
             record.setStartDate(new Date());
         }
+        record.setId(UUID.randomUUID().toString());
         record.setStatus(Status.NEW);
         return this.todoRepository.save(record);
     }
@@ -51,10 +53,29 @@ public class Todo {
         return this.todoRepository.update(record);
     }
 
-    public void delete(String id) {
+    public void deleteTodoRecord(String id) {
 
         // TODO: buscar si el record existe, y si existe borrarlo
         this.todoRepository.remove(id);
+    }
+
+    public List<TodoRecord> getStartDateRange(Date startDate, Date endDate) {
+        // TODO: validamos que startDAte y endDate no sean nulos
+        // TODO: validamos que el endDate es mayor o igual que el startDate
+        // TODO: validamos que las fechas tienen que estar en un rango definido por alguien
+        return this.todoRepository.findByBetweenStartDates(startDate, endDate);
+    }
+
+    public List<TodoRecord> searchInTitle(String textToSearch) {
+        // TODO: validar que el textToSearch no sea nulo ni vacio
+        return this.todoRepository.findByPatternInTitle(textToSearch);
+    }
+
+    public TodoRecord updateTodoRecord(TodoRecord todoRecord) {
+        // TODO: que el ID del todoRecord Exista
+        // TODO: validar que como minimo el todoRecord tenga ID, titulo, startDate y Status
+        // TODO: el titulo, startDate, Status no pueden nulos (o vacios)
+        return this.todoRepository.update(todoRecord);
     }
 
 }
